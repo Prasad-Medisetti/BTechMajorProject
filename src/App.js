ba7979d054a04799f26ac7ffa6505db389963dea
Prasad Medisetti <prasadmedisetti34@gmail.com>
2021-03-23 21:45:23 +0530

    basic home page template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import ReactDOM from "react-dom";
+
+import AppBar from "./components/appBar/appBar.component";
+import Footer from "./components/footer/footer.component";
+
+import SignInPage from "./pages/SignIn/SignIn.page";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<AppBar />
+			<SignInPage />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
