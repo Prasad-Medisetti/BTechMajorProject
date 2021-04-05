bf79d2cc75858fb9999ff42cff025a028ade269f
Prasad Medisetti <prasadmedisetti34@gmail.com>
2021-04-05 06:27:19 +0530

    Test on heroku

--- server/database/db.js
+++ server/database/db.js
@@ -1,3 +1,5 @@
 module.exports = {
-	db: "mongodb://localhost:27017/notesapp",
+	// db: "mongodb://localhost:27017/notesapp",
+	db:
+		"mongodb+srv://Admin:Admin@cluster0.7gwdx.mongodb.net/btech-major-project?retryWrites=true&w=majority",
 };
