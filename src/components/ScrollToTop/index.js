c89ded199700774753041cc827e97c1b1731bb96
Prasad Medisetti <prasadmedisetti34@gmail.com>
2021-05-29 17:31:49 +0530

    scroll to top completed...

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -1,19 +1,45 @@
 import { makeStyles } from "@material-ui/core";
 import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
-import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import KeyboardArrowUpRoundedIcon from "@material-ui/icons/KeyboardArrowUpRounded";
 import React, { useEffect, useState } from "react";
 import { useWindowScroll } from "react-use";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
+	// "@global": {
+	// 	"@keyframes scrollTop": {
+	// 		from: {
+	// 			transform: "translateY(2px)",
+	// 		},
+	// 		to: {
+	// 			transform: "translateY(-2px)",
+	// 		},
+	// 	},
+	// },
+	scroll_to_top: {
 		position: "fixed",
 		bottom: theme.spacing(4),
 		right: theme.spacing(4),
-		background: "#37474f",
+		backgroundColor: "transparent",
+		borderRadius: "50%",
+	},
+	scroll_to_top__icon: {
+		// backgroundColor: "#37474f",
+		backgroundColor: "#424242",
 		color: "#fafafa",
+		padding: "1.5em",
 		"&:hover": {
-			background: "#37474fcc",
+			// backgroundColor: "#455A64",
+			backgroundColor: "#535353",
+		},
+		animation: `$myAnim 0.4s alternate ${theme.transitions.easing.easeInOut} infinite`,
+	},
+	"@keyframes myAnim": {
+		from: {
+			transform: "translateY(3px)",
+		},
+		to: {
+			transform: "translateY(-2px)",
 		},
 	},
 }));
@@ -34,16 +60,18 @@ const ScrollToTop = () => {
 
 	return (
 		<Zoom in={visibility}>
-			<Fab
-				color="inherit"
-				size="small"
-				aria-label="scroll back to top"
-				onClick={backToTop}
-				role="presentation"
-				className={classes.root}
-			>
-				<KeyboardArrowUpIcon />
-			</Fab>
+			<div className={classes.scroll_to_top}>
+				<Fab
+					className={classes.scroll_to_top__icon}
+					color="inherit"
+					size="small"
+					aria-label="scroll back to top"
+					onClick={backToTop}
+					role="presentation"
+				>
+					<KeyboardArrowUpRoundedIcon />
+				</Fab>
+			</div>
 		</Zoom>
 	);
 };
