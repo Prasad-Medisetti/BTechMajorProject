6041b98a9ea95eb43295ebb36f982ab3da8a2e2a
Prasad Medisetti <prasadmedisetti34@gmail.com>
2021-06-14 17:58:39 +0530

    added forgot and already have account buttons

--- src/components/Snackbar/index.js
+++ src/components/Snackbar/index.js
@@ -2,45 +2,47 @@ import { IconButton } from "@material-ui/core";
 import Slide from "@material-ui/core/Slide";
 import Snackbar from "@material-ui/core/Snackbar";
 import CloseIcon from "@material-ui/icons/Close";
-import { Alert } from "@material-ui/lab";
 import React from "react";
+import { Alert } from "@material-ui/lab";
+// import { AlertTitle } from "@material-ui/lab";
+// import { titleCase } from "../../utils";
 
 function TransitionDown(props) {
-	return <Slide {...props} direction="down" />;
+  return <Slide {...props} direction="down" />;
 }
 
 export default function MySnackbar(props) {
+  // console.log(`toast`, props.toast)
 
-	// console.log(`toast`, props.toast)
-
-	return (
-		<Snackbar
-			open={props.toast.toastOpen}
-			style={{ margin: "4.5rem auto" }}
-			anchorOrigin={{ vertical: "top", horizontal: "center" }}
-			TransitionComponent={TransitionDown}
-			key="down"
-		>
-			<Alert
-				variant={props.toast.toastVariant}
-				severity={props.toast.toastColor}
-				action={
-					<IconButton
-						key="close"
-						aria-label="close"
-						color="inherit"
-						size="small"
-						onClick={props.toast.handleToastClose}
-					>
-						<CloseIcon />
-					</IconButton>
-				}
-				onClose={props.toast.handleToastClose}
-			>
-				{props.toast.toastMessage !== ""
-					? props.toast.toastMessage
-					: "This is a toast"}
-			</Alert>
-		</Snackbar>
-	);
+  return (
+    <Snackbar
+      open={props.toast.toastOpen}
+      style={{ margin: "4.5rem auto" }}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      TransitionComponent={TransitionDown}
+      key="down"
+    >
+      <Alert
+        variant={props.toast.toastVariant}
+        severity={props.toast.toastColor}
+        action={
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={props.toast.handleToastClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        }
+        onClose={props.toast.handleToastClose}
+      >
+        {/* <AlertTitle>{titleCase(props.toast.toastColor)}</AlertTitle> */}
+        {props.toast.toastMessage !== ""
+          ? props.toast.toastMessage
+          : "This is a toast"}
+      </Alert>
+    </Snackbar>
+  );
 }
--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-//local, dev, sit, uat
 const instance = axios.create({
-	baseURL: "https://academic-bulletin-board.herokuapp.com/",
-  	headers: {
-    		'Access-Control-Allow-Origin' : '*',
-    		'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    	}
+  baseURL: "https://academic-bulletin-board.herokuapp.com/",
+  // baseURL: "http://localhost:4000",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  }
 });
 
 export default instance;
